0e8ff031e0530308ffb6830b4998db214d484e45
ralphjacobs <ralphjacobs@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-06-11 17:06:56 +0000

    Integration test changes

--- collective/classifieds/tests/base.py
+++ collective/classifieds/tests/base.py
@@ -2,18 +2,14 @@
 
 from Products.Five import zcml
 from Products.Five import fiveconfigure
-
 from Testing import ZopeTestCase as ztc
-
 from Products.PloneTestCase import PloneTestCase as ptc
 from Products.PloneTestCase.layer import onsetup
-
 import os
 
 @onsetup
 def setup_product():
     """Set up the package and its dependencies."""
-    
     fiveconfigure.debug_mode = True
     import collective.classifieds
     zcml.load_config('configure.zcml', collective.classifieds)
