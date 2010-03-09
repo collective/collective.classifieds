cb4fd31ea4f51bcac8291baec7eb90a65bf5bedd
ralphjacobs <ralphjacobs@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-03-09 18:55:15 +0000

    1.5rc1 release candidate

--- collective/classifieds/config.py
+++ collective/classifieds/config.py
@@ -12,22 +12,15 @@ ADD_CONTENT_PERMISSIONS = {
     'ClassifiedsCategory': 'Classifieds: Add ClassifiedsCategory',
     'Classifieds': 'Classifieds: Add Classifieds',
     'Classified': 'Classifieds: Add Classified',
-    'OrderedClassifiedsCategory':
-        'Classifieds: Add OrderedClassifiedsCategory',
+    'OrderedClassifiedsCategory': 'Classifieds: Add OrderedClassifiedsCategory',
 }
 
 setDefaultRoles('Classifieds: Add ClassifiedsCategory', ('Manager', 'Owner'))
 setDefaultRoles('Classifieds: Add Classifieds', ('Manager', 'Owner'))
 setDefaultRoles('Classifieds: Add Classified', ('Manager', 'Owner'))
-setDefaultRoles('Classifieds: Add OrderedClassifiedsCategory',
-                ('Manager', 'Owner'))
+setDefaultRoles('Classifieds: Add OrderedClassifiedsCategory', ('Manager', 'Owner'))
 
 product_globals = globals()
 
-# Dependencies of Products to be installed by quick-installer
-# override in custom configuration
 DEPENDENCIES = []
-
-# Dependend products - not quick-installed - used in testcase
-# override in custom configuration
 PRODUCT_DEPENDENCIES = []
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.5b4'
+version = '1.5rc1'
 
 setup(name='collective.classifieds',
       version=version,
