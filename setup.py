0e686fedfbf7a5349cedd8beb4674f3179c555cd
hvelarde <hector.velarde@gmail.com>
2014-05-05 15:11:50 -0300

    Fix package distribution

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
 from setuptools import setup, find_packages
-import os
 
 version = '1.6'
+long_description = (
+    open('README.rst').read() + '\n' +
+    # open('CONTRIBUTORS.rst').read() + '\n' +
+    open('CHANGES.rst').read()
+)
 
 setup(name='collective.classifieds',
       version=version,
       description="Add classifieds to your Plone intranet or website.",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      long_description=long_description,
       classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
+          'Framework :: Plone',
+          'Programming Language :: Python',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
       keywords='classifieds sales intranet',
       author='Four Digits (ralph@fourdigits.nl)',
       author_email='ralph@fourdigits.nl',
